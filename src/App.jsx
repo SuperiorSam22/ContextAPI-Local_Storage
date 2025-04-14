78f35985515ad15eb55102bbefc0947821f368b8
Sandeep Lal <sandeep221102@gmail.com>
2025-04-14 14:49:31 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { TodoProvider } from "./contexts";
+import { TodoForm, TodoItem } from "./components";
 
 function App() {
-  const [toods, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
   const addTodo = (todo) => {
     setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
@@ -22,7 +23,7 @@ function App() {
   const toggleComplete = (id) => {
     setTodos((prev) =>
       prev.map((prevTodo) =>
-        prevTodo === id
+        prevTodo.id === id
           ? { ...prevTodo, completed: !prevTodo.completed }
           : prevTodo
       )
@@ -33,13 +34,13 @@ function App() {
     const todos = JSON.parse(localStorage.getItem("todos"));
 
     if (todos && todos.length > 0) {
-      setTodos(todos)
+      setTodos(todos);
     }
   }, []);
 
   useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos))
-  }, [todos]) 
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <TodoProvider
@@ -50,9 +51,18 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">
+            <TodoForm/>
+          </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id}
+              className="w-full"
+              >
+                <TodoItem todo={todo }/>
+              </div>
+            ))}
           </div>
         </div>
       </div>
